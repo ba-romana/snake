0a8ed4de6a4d5519b94febbd8a6a4e364428506d
Romana Babicova <romana.babicova@pelicantravel.local>
2025-05-11 16:01:30 +0200

    oddelila som css z index.html

--- main.js
+++ main.js
@@ -1,201 +1,201 @@
 // listeners
-        document.addEventListener('keydown', keyPush)
-        document.querySelectorAll('input[type="radio"]').forEach(input =>
-            input.addEventListener('change', gameOptions)
-        )
+document.addEventListener('keydown', keyPush)
+document.querySelectorAll('input[type="radio"]').forEach(input =>
+    input.addEventListener('change', gameOptions)
+)
         
         
-        //canvas
-        const canvas = document.querySelector('canvas');
-        const title = document.querySelector('h1');
-        const ctx = canvas.getContext('2d');
+//canvas
+const canvas = document.querySelector('canvas');
+const title = document.querySelector('h1');
+const ctx = canvas.getContext('2d');
 
-        //game 
+//game 
 
-        let gameIsRunning = true;
-        let gameSpeedMode = "slow";
+let gameIsRunning = true;
+let gameSpeedMode = "slow";
 
-        let fps = 10;
-        let gameInterval;
+let fps = 10;
+let gameInterval;
 
-        const tailSize = 50;
-        const tileCountX = canvas.width / tailSize;
-        const tileCountY = canvas.height / tailSize;
+const tailSize = 50;
+const tileCountX = canvas.width / tailSize;
+const tileCountY = canvas.height / tailSize;
 
-        let score = 0;
+let score = 0;
 
-        //player
-        let snakeSpeed = tailSize;
-        let snakePosX = 0;
-        let snakePosY = canvas.height/2;
-       
-
-        let velocityX = 0;
-        let velocityY = 0;
-
-        let tail = [];
-        let snakeLength = 2;
-
-        let foodPosX = 0;
-        let foodPosY = 0;
+//player
+let snakeSpeed = tailSize;
+let snakePosX = 0;
+let snakePosY = canvas.height/2;
 
 
-        //loop
-        function gameLoop() {
-            if (gameIsRunning) {
-                drawStuff();
-                moveStuff(); 
-            }
+let velocityX = 0;
+let velocityY = 0;
+
+let tail = [];
+let snakeLength = 2;
+
+let foodPosX = 0;
+let foodPosY = 0;
 
 
-        };
+//loop
+function gameLoop() {
+    if (gameIsRunning) {
+        drawStuff();
+        moveStuff(); 
+    }
 
-        resetFood();
-        startGame();
-        gameLoop(); 
 
-        function moveStuff(){            
-            snakePosX += snakeSpeed * velocityX;
-            snakePosY += snakeSpeed * velocityY;
+};
 
-            //wall collision
-            if (snakePosX > (canvas.width - tailSize)) {
-                snakePosX = 0
-            }
-            if (snakePosX < 0) {
-                snakePosX = canvas.width
-            }
-            if (snakePosY > (canvas.height - tailSize)) {
-                snakePosY = 0
-            }
-            if (snakePosY < 0) {
-                snakePosY = canvas.height
-            }
+resetFood();
+startGame();
+gameLoop(); 
 
-            if (velocityX !== 0 || velocityY !== 0) {
-                tail.forEach(snakePart => {
-                    if (snakePosX === snakePart.x && snakePosY === snakePart.y) {
-                        gameOver();
-                    }
-                });
-            }
+function moveStuff(){            
+    snakePosX += snakeSpeed * velocityX;
+    snakePosY += snakeSpeed * velocityY;
 
-            tail.push({ x: snakePosX, y: snakePosY })
-            tail = tail.slice (-1*snakeLength)
- 
-            //food collision
-            
-            
-            if (snakePosX === foodPosX && snakePosY === foodPosY){
-                title.textContent = ++score;
-                snakeLength++;
-                resetFood();
+    //wall collision
+    if (snakePosX > (canvas.width - tailSize)) {
+        snakePosX = 0
+    }
+    if (snakePosX < 0) {
+        snakePosX = canvas.width
+    }
+    if (snakePosY > (canvas.height - tailSize)) {
+        snakePosY = 0
+    }
+    if (snakePosY < 0) {
+        snakePosY = canvas.height
+    }
 
-                if (gameSpeedMode === "fast") {
-                    fps+=1;
-                    startGame();
-                }
-            }   
-        }
-
-        function drawStuff(){
-            rectangle('#123456', 0, 0, canvas.width, canvas.height);
-
-            drawGrid();
-
-            rectangle('yellow', foodPosX, foodPosY, tailSize, tailSize);
-
-            tail.forEach ( snakePart => 
-                rectangle('#555555', snakePart.x, snakePart.y, tailSize, tailSize)
-            )
-
-            rectangle('black', snakePosX, snakePosY, tailSize, tailSize);
-        }
-
-        function rectangle(color, x, y, width, height){
-            ctx.fillStyle = color;
-            ctx.fillRect(x, y, width, height);
-        }
-
-        function resetFood() {
-            if (snakeLength === tileCountX*tileCountY) {
+    if (velocityX !== 0 || velocityY !== 0) {
+        tail.forEach(snakePart => {
+            if (snakePosX === snakePart.x && snakePosY === snakePart.y) {
                 gameOver();
             }
+        });
+    }
 
-            foodPosX = Math.floor(Math.random() * tileCountX) * tailSize;
-            foodPosY = Math.floor(Math.random() * tileCountY) * tailSize;
+    tail.push({ x: snakePosX, y: snakePosY })
+    tail = tail.slice (-1*snakeLength)
 
-            if (foodPosX ===snakePosX && foodPosY === snakePosY ) {
-                resetFood();
-            };
+    //food collision
+    
+    
+    if (snakePosX === foodPosX && snakePosY === foodPosY){
+        title.textContent = ++score;
+        snakeLength++;
+        resetFood();
 
-            if (tail.some(snakePart => snakePart.x === foodPosX && snakePart.y === foodPosY)) {
-                resetFood();
-            };
-        }
-
-
-        function startGame() {
-            if (gameInterval) clearInterval(gameInterval);
-            gameInterval = setInterval(gameLoop, 1000/fps)
-        }
-
-        function gameOver() {
-            title.innerHTML = `<strong>☠ ${score} ☠</strong>`
-            gameIsRunning = false;
-        }
-        
-        function keyPush(event){
-            switch(event.key) {
-                case 'ArrowUp':
-                    if (velocityY !== 1) {
-                    velocityX = 0;
-                    velocityY = -1;
-                    }
-                    break;
-                case 'ArrowDown':
-                    if (velocityY !== -1) {
-                    velocityX = 0;
-                    velocityY = 1;
-                    }
-                    break;
-                case 'ArrowLeft':
-                    if (velocityX !== 1) {
-                    velocityX = -1;
-                    velocityY = 0;
-                    }
-                    break;        
-                case 'ArrowRight':
-                    if (velocityX !== -1) {
-                    velocityX = 1;
-                    velocityY = 0;
-                    }
-                    break;
-
-                default: 
-                    if (! gameIsRunning ) location.reload();
-                    break;
-            }
-        }
-
-        function gameOptions(event){
-            const velocitySelection = document.querySelector('.velocityStyle input[type="radio"]:checked');
-            const gameboardSelection = document.querySelector('.gameboardStyle input[type="radio"]:checked');
-
-            const velocityStyle = velocitySelection.value; // slow or fast
-                fps = 10;
-                gameSpeedMode = velocityStyle;
-                startGame();
-
-            const gameboardStyle = gameboardSelection.value; //infinite or square
-
+        if (gameSpeedMode === "fast") {
+            fps+=1;
             startGame();
         }
+    }   
+}
 
-        function drawGrid(){
-             for (let i = 0; i < tileCountX; i++) {
-                for (let j = 0; j < tileCountY; j++) {
-                rectangle('#ffffff', tailSize * i, tailSize * j, tailSize -1, tailSize-1)
-                }
-            } 
+function drawStuff(){
+    rectangle('#123456', 0, 0, canvas.width, canvas.height);
+
+    drawGrid();
+
+    rectangle('yellow', foodPosX, foodPosY, tailSize, tailSize);
+
+    tail.forEach ( snakePart => 
+        rectangle('#555555', snakePart.x, snakePart.y, tailSize, tailSize)
+    )
+
+    rectangle('black', snakePosX, snakePosY, tailSize, tailSize);
+}
+
+function rectangle(color, x, y, width, height){
+    ctx.fillStyle = color;
+    ctx.fillRect(x, y, width, height);
+}
+
+function resetFood() {
+    if (snakeLength === tileCountX*tileCountY) {
+        gameOver();
+    }
+
+    foodPosX = Math.floor(Math.random() * tileCountX) * tailSize;
+    foodPosY = Math.floor(Math.random() * tileCountY) * tailSize;
+
+    if (foodPosX ===snakePosX && foodPosY === snakePosY ) {
+        resetFood();
+    };
+
+    if (tail.some(snakePart => snakePart.x === foodPosX && snakePart.y === foodPosY)) {
+        resetFood();
+    };
+}
+
+
+function startGame() {
+    if (gameInterval) clearInterval(gameInterval);
+    gameInterval = setInterval(gameLoop, 1000/fps)
+}
+
+function gameOver() {
+    title.innerHTML = `<strong>☠ ${score} ☠</strong>`
+    gameIsRunning = false;
+}
+
+function keyPush(event){
+    switch(event.key) {
+        case 'ArrowUp':
+            if (velocityY !== 1) {
+            velocityX = 0;
+            velocityY = -1;
+            }
+            break;
+        case 'ArrowDown':
+            if (velocityY !== -1) {
+            velocityX = 0;
+            velocityY = 1;
+            }
+            break;
+        case 'ArrowLeft':
+            if (velocityX !== 1) {
+            velocityX = -1;
+            velocityY = 0;
+            }
+            break;        
+        case 'ArrowRight':
+            if (velocityX !== -1) {
+            velocityX = 1;
+            velocityY = 0;
+            }
+            break;
+
+        default: 
+            if (! gameIsRunning ) location.reload();
+            break;
+    }
+}
+
+function gameOptions(event){
+    const velocitySelection = document.querySelector('.velocityStyle input[type="radio"]:checked');
+    const gameboardSelection = document.querySelector('.gameboardStyle input[type="radio"]:checked');
+
+    const velocityStyle = velocitySelection.value; // slow or fast
+        fps = 10;
+        gameSpeedMode = velocityStyle;
+        startGame();
+
+    const gameboardStyle = gameboardSelection.value; //infinite or square
+
+    startGame();
+}
+
+function drawGrid(){
+        for (let i = 0; i < tileCountX; i++) {
+        for (let j = 0; j < tileCountY; j++) {
+        rectangle('#ffffff', tailSize * i, tailSize * j, tailSize -1, tailSize-1)
         }
+    } 
+}
